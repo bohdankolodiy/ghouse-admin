17dc4c8dbbfdd1e5e2eaffaf750e7da466b8af2c
bkolodiy <bkolodiy@binariks.com>
2025-03-06 23:26:56 +0200

    Try to deploy whole app

--- medusa-config.ts
+++ medusa-config.ts
@@ -9,15 +9,14 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: "*", // Дозволяє запити з будь-якого домену (тимчасове рішення)
-      adminCors:
-        process.env.ADMIN_CORS || "https://ghouse-admin-nine.vercel.app",
-
-      authCors: process.env.AUTH_CORS || "https://ghouse-admin-nine.vercel.app",
+      adminCors: process.env.MEDUSA_BACKEND_URL ?? process.env.ADMIN_CORS!,
+      authCors: process.env.MEDUSA_BACKEND_URL ?? process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
   admin: {
-    disable: true,
+    disable: false,
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
 });
