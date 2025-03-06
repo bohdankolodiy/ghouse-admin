27e996bb1953fe7e0c9e343200cd1fb6db2dabaf
bkolodiy <bkolodiy@binariks.com>
2025-03-06 21:19:24 +0200

    Revert "Update config"
    This reverts commit 41a773c00064618745cca01d01f1a99efe317e8a.

--- medusa-config.ts
+++ medusa-config.ts
@@ -15,9 +15,7 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  // admin: {
-  //   disable: process.env.MEDUSA_DISABLE_ADMIN === "true" || false,
-  //   backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
-  //   path: (process.env.MEDUSA_ADMIN_PATH || "/app") as `/${string}`,
-  // },
+  admin: {
+    disable: true
+  },
 });
